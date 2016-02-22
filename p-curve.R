437507d767206c9bc5d36ff17ba3defb817357e3
Felix Schönbrodt <felix@nicebread.de>
2016-02-22 13:47:21 +0100

    v0.6 release

--- p-curve.R
+++ p-curve.R
@@ -161,182 +161,20 @@ p_curve_2 <- function(pps) {
 
 
 
-# # ---------------------------------------------------------------------
-# # Compute green 33%-curve of plot
-#
-# #8 Green line (Expected p-curve for 33% power)
-#      #8.1 t-tests
-#     if (length(t.value.sig)>0)  #if nonempty compute pp-values
-#     {
-#           #Critical values,xc, for p=.05, .04, .03, .02 and ,01
-#               t.x5=qt(.975,df=t.df.sig); t.x4=qt(.98, df=t.df.sig); t.x3=qt(.985,df=t.df.sig);
-#               t.x2=qt(.99, df=t.df.sig); t.x1=qt(.995,df=t.df.sig)
-#           #For Binomial test
-# 	  	      t.x25=qt(.9875,df=t.df.sig)                            #critical value for t-tests to get p=.025
-# 			      t.plow=1- 3*(pt(t.x25,df=t.df.sig, ncp=t.ncp33)-2/3)   #prob(p<.025 | ncp33% & p<.05)
-#
-#           #Probabilty of a p-value bigger  p=.05, .04, .03, .02 and .01 given p<.05 and ncp=ncp33
-#               t.pp4=3*(pt(t.x4,df=t.df.sig, ncp=t.ncp33)-2/3)
-#               t.pp3=3*(pt(t.x3,df=t.df.sig, ncp=t.ncp33)-2/3)
-#               t.pp2=3*(pt(t.x2,df=t.df.sig, ncp=t.ncp33)-2/3)
-#               t.pp1=3*(pt(t.x1,df=t.df.sig, ncp=t.ncp33)-2/3)
-#          #within bins proportions
-#               t.prop5=mean(t.pp4);
-#               t.prop4=mean(t.pp3-t.pp4);
-#               t.prop3=mean(t.pp2-t.pp3);
-#               t.prop2=mean(t.pp1-t.pp2);
-#               t.prop1=mean(1-t.pp1)
-#     }
-#     #8.2 f-tests
-#           if (length(f.value.sig)>0)  #if nonempty compute pp-values
-#           {
-#           #Critical values,xc, for p=.05, .04, .03, .02 and ,01
-#               f.x5=qf(.95,df1=f.df1.sig, df2=f.df2.sig);  f.x4=qf(.96,df1=f.df1.sig, df2=f.df2.sig); f.x3=qf(.97,,df1=f.df1.sig, df2=f.df2.sig);
-#               f.x2=qf(.98, df1=f.df1.sig, df2=f.df2.sig); f.x1=qf(.99,df1=f.df1.sig, df2=f.df2.sig)
-#           #For binomial test
-# 			        f.x25 =qf(.975,df1=f.df1.sig, df2=f.df2.sig)                          #Critical F value for p=.025
-#               f.plow=1-3*(pf(f.x25,df1=f.df1.sig, df2=f.df2.sig, ncp=f.ncp33)-2/3)  #Prob(p<.025|ncp33% & p<.05)
-#
-#
-#           #Probabilty of a p-value bigger  p=.05, .04, .03, .02 and .01 given p<.05 and ncp=ncp33
-#               f.pp4=3*(pf(f.x4,df1=f.df1.sig, df2=f.df2.sig, ncp=f.ncp33)-2/3)
-#               f.pp3=3*(pf(f.x3,df1=f.df1.sig, df2=f.df2.sig, ncp=f.ncp33)-2/3)
-#               f.pp2=3*(pf(f.x2,df1=f.df1.sig, df2=f.df2.sig, ncp=f.ncp33)-2/3)
-#               f.pp1=3*(pf(f.x1,df1=f.df1.sig, df2=f.df2.sig, ncp=f.ncp33)-2/3)
-#
-#           #within bins proportions
-#               f.prop5=mean(f.pp4);
-#               f.prop4=mean(f.pp3-f.pp4);
-#               f.prop3=mean(f.pp2-f.pp3);
-#               f.prop2=mean(f.pp1-f.pp2);
-#               f.prop1=mean(1-f.pp1)
-#           }
-#     #8.3 chi-tests
-#         if (length(c.value.sig)>0)  #if nonempty compute pp-values
-#         {
-#
-#           #Critical values,xc, for p=.05, .04, .03, .02 and ,01
-#               c.x5=qchisq(.95,df=c.df.sig); c.x4=qchisq(.96, df=c.df.sig); c.x3=qchisq(.97,df=c.df.sig);
-#               c.x2=qchisq(.98, df=c.df.sig); c.x1=qchisq(.99,df=c.df.sig)
-#
-# 		  #For binomial test
-# 		      c.x25 =qchisq(.975,df=c.df.sig)                                      #Critical x2 value for p=.025
-# 	          c.plow=1-3*(pchisq(c.x25,df=c.df.sig, ncp=c.ncp33)-2/3)              #Prob(p<.025|ncp33% & p<.05)
-#
-# 			  #Probabilty of a p-value bigger  p=.05, .04, .03, .02 and .01 given p<.05 and ncp=ncp33
-#               c.pp4=3*(pchisq(c.x4,df=c.df.sig, ncp=c.ncp33)-2/3)
-#               c.pp3=3*(pchisq(c.x3,df=c.df.sig, ncp=c.ncp33)-2/3)
-#               c.pp2=3*(pchisq(c.x2,df=c.df.sig, ncp=c.ncp33)-2/3)
-#               c.pp1=3*(pchisq(c.x1,df=c.df.sig, ncp=c.ncp33)-2/3)
-#
-#           #within bins proportions
-#               c.prop5=mean(c.pp4); c.prop4=mean(c.pp3-c.pp4); c.prop3=mean(c.pp2-c.pp3);
-#               c.prop2=mean(c.pp1-c.pp2); c.prop1=mean(1-c.pp1)
-#         }
-#     #8.4 z-tests
-#       if (length(z.value.sig)>0)  #if nonempty compute pp-values
-#       {
-#           #Critical values,xc, for p=.05, .04, .03, .02 and ,01
-#               z.x5=qnorm(.975); z.x4=qnorm(.98); z.x3=qnorm(.985); z.x2=qnorm(.99); z.x1=qnorm(.995)
-# 		  # For Binomial test
-# 			    z.x25 =qnorm(.9825)                                         #Critical x2 value for p=.025
-# 		      z.plow=1-3*(pnorm(z.x25,mean=1.5285687,sd=1)-2/3)           #Prob(p<.025|ncp33% & p<.05)
-#         #Probabilty of a p-value bigger  p=.05, .04, .03, .02 and .01, given p<.05 and ncp=ncp33
-#               z.pp4=3*(pnorm(z.x4,mean=1.5285687,sd=1)-2/3)
-#               z.pp3=3*(pnorm(z.x3,mean=1.5285687,sd=1)-2/3)
-#               z.pp2=3*(pnorm(z.x2,mean=1.5285687,sd=1)-2/3)
-#               z.pp1=3*(pnorm(z.x1,mean=1.5285687,sd=1)-2/3)
-#         #within bins proportions
-#               z.prop5=z.pp4; z.prop4=z.pp3-z.pp4; z.prop3=z.pp2-z.pp3; z.prop2=z.pp1-z.pp2; z.prop1=1-z.pp1
-#       }
-#
-#
-# #9 combine t,F,chi,Z
-#           #proportion of all tests that are of each type
-#             t.share=length(t.value.sig)/ktot
-#             f.share=length(f.value.sig)/ktot
-#             c.share=length(c.value.sig)/ktot
-#             z.share=length(z.value.sig)/ktot
-#
-#           #Average proportions within the 4 types of tests
-#             t.props=c(t.prop1, t.prop2, t.prop3, t.prop4, t.prop5)
-#             f.props=c(f.prop1, f.prop2, f.prop3, f.prop4, f.prop5)
-#             c.props=c(c.prop1, c.prop2, c.prop3, c.prop4, c.prop5)
-#             z.props=c(z.prop1, z.prop2, z.prop3, z.prop4, z.prop5)
-#
-#           #overall proportions (i.e.., THE GREEN LINE)
-#             green=100*(t.props*t.share + f.props*f.share + c.props*c.share + z.props*z.share)
-#
-
-
-get_33_curve <- function(type, statistic, df, df2, p.crit=.05, power=1/3) {
-
-    # convert r to t values
-	type <- as.character(type)
-	statistic[tolower(type)=="r"] <- statistic[tolower(type)=="r"] / sqrt( (1 - statistic[tolower(type)=="r"]^2) / df[tolower(type)=="r"])
-	type[tolower(type)=="r"] <- "t"
-
-	statistic <- abs(statistic)
-
-type <- rep("f", 3)
-statistic <- c(5.1, 6.3, 7.1)
-df <- c(1, 1, 1)
-df2 <- c(88, 100, 200)
-
-	ncp <- get_pp_values(type=type, statistic=statistic, df=df, df2=df2, p.crit=.05, power=1/3)$ncp
-
-	res <- data.frame()
+theoretical_power_curve <- function(power=1/3, p.max=.05, normalize=TRUE) {
+	# compute arbitrary test statistics for requested power
+	library(pwr)
+	d <- 0.2
+	n <- pwr.t.test(d=0.2, power=power)$n*2
 	
-	# ---------------------------------------------------------------------
-	# t-values
-	# Critical values,xc, for p=.05, .04, .03, .02 and ,01
-	t.crit <- list()
-	t.CRIT <- c(.975, .98, .985, .99, .995)
-	for (j in 1:5) 
-		t.crit[[j]] <- qt(t.CRIT[j], df=df[type=="t"])
-
-	# Probability of a p-value bigger  p=.05, .04, .03, .02 and .01 given p<.05 and ncp=ncp33
-	t.pp <- c()
-	for (j in 1:5)
-		t.pp[j] <- mean((1/power)*(pt(t.crit[[j]], df=df[type=="t"], ncp=ncp[ncp$type=="t", "ncp"])-(1-power)))
-
-	t.pp[1] <- 0
-	t.pp <- c(t.pp, 1)
-	t.prop <- t.pp[2:6]-t.pp[1:5]
-	
-	# ---------------------------------------------------------------------
-	# F-values
-	# Critical values,xc, for p=.05, .04, .03, .02 and ,01
-	f.crit <- list()
-	f.CRIT <- c(.95, .96, .97, .98, .99)
-	for (j in 1:5) 
-		f.crit[[j]] <- qf(f.CRIT[j], df1=df[type=="f"], df2=df2[type=="f"])
-
-	# Probability of a p-value bigger  p=.05, .04, .03, .02 and .01 given p<.05 and ncp=ncp33
-	f.pp <- c()
-	for (j in 1:5)
-		f.pp[j] <- mean((1/power)*(pf(f.crit[[j]], df1=df[type=="f"], df2=df2[type=="f"], ncp=ncp[ncp$type=="f", "ncp"])-(1-power)))
-
-	f.pp[1] <- 0
-	f.pp <- c(f.pp, 1)
-	f.prop <- f.pp[2:6]-f.pp[1:5]
-
-	# ---------------------------------------------------------------------
-	# chi2-values
-	# Critical values,xc, for p=.05, .04, .03, .02 and ,01
-	chi.crit <- list()
-	chi.CRIT <- c(.95, .96, .97, .98, .99)
-	for (j in 1:5) 
-		chi.crit[[j]] <- qt(chi.CRIT[j], df=df[type=="chi2"])
-
-	# Probability of a p-value bigger  p=.05, .04, .03, .02 and .01 given p<.05 and ncp=ncp33
-	chi.pp <- c()
-	for (j in 1:5)
-		chi.pp[j] <- mean((1/power)*(pchisq(chi.crit[[j]], df=df[type=="chi2"], ncp=ncp[ncp$type=="chi2", "ncp"])-(1-power)))
-
-	chi.pp[1] <- 0
-	chi.pp <- c(chi.pp, 1)
-	chi.prop <- chi.pp[2:6]-chi.pp[1:5]
-	
-	#TODO: z-values!
+	crit <- seq(0.01, p.max, by=.01)
+	pdens <- c()
+	for (cr in crit) {
+		pdens <- c(pdens, pwr.t.test(d=0.2, power=NULL, n=n/2, sig.level=cr)$power)
+	}
+	p.dens <- diff(c(0, pdens))
+	if (normalize == TRUE) p.dens <- p.dens/sum(p.dens)
+		
+	names(p.dens) <- as.character(crit)
+	return(p.dens)
 }
